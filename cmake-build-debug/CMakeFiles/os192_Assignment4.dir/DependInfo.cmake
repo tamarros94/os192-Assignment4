4e7aa39b9f743944ecfc225402634a3870fdf708
tamar <rosent@post.bgu.ac.il>
2019-06-15 12:46:14 +0300

    init_proc_pid_dirents done next: handle pid files

---
+++ cmake-build-debug/CMakeFiles/os192_Assignment4.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tamar/os192/Assignment4/os192-Assignment4/bio.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/bio.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/bootmain.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/bootmain.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/cat.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/cat.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/console.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/console.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/echo.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/echo.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/exec.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/exec.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/file.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/file.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/forktest.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/forktest.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/fs.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/fs.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/grep.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/grep.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/ide.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/ide.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/init.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/init.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/ioapic.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/ioapic.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/kalloc.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/kalloc.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/kbd.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/kbd.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/kill.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/kill.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/lapic.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/lapic.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/ln.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/ln.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/log.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/log.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/ls.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/ls.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/main.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/main.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/memide.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/memide.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/mkdir.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/mkdir.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/mkfs.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/mkfs.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/mp.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/mp.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/picirq.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/picirq.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/pipe.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/pipe.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/printf.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/printf.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/proc.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/proc.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/procfs.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/procfs.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/rm.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/rm.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/sh.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/sh.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/sleeplock.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/sleeplock.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/spinlock.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/spinlock.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/stressfs.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/stressfs.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/string.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/string.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/syscall.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/syscall.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/sysfile.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/sysfile.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/sysproc.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/sysproc.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/trap.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/trap.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/uart.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/uart.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/ulib.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/ulib.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/umalloc.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/umalloc.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/usertests.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/usertests.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/vm.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/vm.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/wc.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/wc.c.o"
+  "/home/tamar/os192/Assignment4/os192-Assignment4/zombie.c" "/home/tamar/os192/Assignment4/os192-Assignment4/cmake-build-debug/CMakeFiles/os192_Assignment4.dir/zombie.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/os192_Assignment4.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for os192_Assignment4.
+# This may be replaced when dependencies are built.
